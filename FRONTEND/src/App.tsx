d91f216f927bb1a5e3e65d617488f520dcf44426
skillradius360 <syncmaster420l@gmail.com>
2025-04-19 05:04:36 +0530

    working v1

--- FRONTEND/src/App.tsx
+++ FRONTEND/src/App.tsx
@@ -1,44 +1,59 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState,useRef } from 'react';
 
 export default function MyCanvasComponent() {
-  type dimensionsType = {
-    x:number
-    y:number
-    height:number
-    width:number
-  }
-const [mouseCoord,setMouseCoord]= useState<{clientX:number,clientY:number}>({clientX:0,clientY:0})
+
+const mouseCoord= useRef<{clientX:number,clientY:number}>({clientX:0,clientY:0})
 const [painting,setPainting]= useState(false)
 
 
-    
-    
-    useEffect(()=>{
-  document?.querySelector("#myCanvas")?.addEventListener("mousedown",()=>{
+useEffect(()=>{
+  const canvasPointer = document?.querySelector("#myCanvas")
+  
+
+  function startTracking(e:MouseEvent){
+    e.preventDefault()
     setPainting(true)
-  })
-  
-  document?.querySelector("#myCanvas")?.addEventListener("mouseup",()=>{
+  }
+  function stopTracking(e){
+    e.preventDefault()
     setPainting(false)
-  })
-  
-  document?.getElementById("#myCanvas")?.addEventListener("mousemove",(e)=>{
-    if(!painting) return 
-    const c = document.getElementById("myCanvas") as HTMLCanvasElement;
-    const ctx = c?.getContext("2d");
+  }
+  function draw(e){
+    e.preventDefault()
+    if(painting==false) return 
+    const ctx = canvasPointer.getContext("2d") 
     ctx.lineWidth=10
     ctx.lineCap="round"
-    ctx.lineTo(e.clientX,e.clientY)
-    ctx.stroke()
-    console.log(e)
-  })
+    ctx.beginPath();
+    ctx.moveTo(mouseCoord.current.clientX, mouseCoord.current.clientY);
+    ctx.lineTo(e.clientX, e.clientY);
+    ctx.stroke();
+
+    mouseCoord.current={clientX:e.clientX,clientY:e.clientY}
+    console.log(mouseCoord.current, painting)
+  }
+ 
+
+  canvasPointer?.addEventListener("mousedown",startTracking)
   
-},[mouseCoord,painting])
+  canvasPointer?.addEventListener("mouseup",stopTracking)
+  
+  canvasPointer?.addEventListener("mousemove",draw)
+  
+  return()=>{
+    canvasPointer?.removeEventListener("mousedown",startTracking)
+  
+    canvasPointer?.removeEventListener("mouseup",stopTracking)
+    
+    canvasPointer?.removeEventListener("mousemove",draw)
+  }
+},[painting])
   
 
   return (
     <div>
-      <canvas className="h-[768px] w-[1366px] ring-2 ring-amber-500 m-32" id="myCanvas" ></canvas>
+      <canvas  width={1080}
+  height={1920}className="h-[1920px] w-[1080px] ring-2 ring-amber-500 m-32" id="myCanvas" ></canvas>
     </div>
   );
 }
