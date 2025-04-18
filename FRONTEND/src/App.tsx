a4ce4009a968525745f76849467b88e8f4f16251
skillradius360 <syncmasterx@gmail.com>
2025-04-18 23:12:10 +0530

    added somewhere

--- FRONTEND/src/App.tsx
+++ FRONTEND/src/App.tsx
@@ -2,51 +2,43 @@ import { useEffect, useState } from 'react';
 
 export default function MyCanvasComponent() {
   type dimensionsType = {
-    x1:number
-    y1:number
-    x2:number
-    y2:number
+    x:number
+    y:number
+    height:number
+    width:number
   }
-  const [dimensions,setDimesions]= useState<dimensionsType>({x1:0,y1:0,x2:0,y2:0})
-const [mouseCoord,setMouseCoord]= useState<{x:number,y:number}>({x:0,y:0})
+const [mouseCoord,setMouseCoord]= useState<{clientX:number,clientY:number}>({clientX:0,clientY:0})
+const [painting,setPainting]= useState(false)
 
-  useEffect(() => {
 
+    
+    
+    useEffect(()=>{
+  document?.querySelector("#myCanvas")?.addEventListener("mousedown",()=>{
+    setPainting(true)
+  })
+  
+  document?.querySelector("#myCanvas")?.addEventListener("mouseup",()=>{
+    setPainting(false)
+  })
+  
+  document?.getElementById("#myCanvas")?.addEventListener("mousemove",(e)=>{
+    if(!painting) return 
     const c = document.getElementById("myCanvas") as HTMLCanvasElement;
-    const ctx = c.getContext("2d");
-    ctx?.strokeRect(dimensions.x1,dimensions.y1,mouseCoord.x,mouseCoord.y)
-
-  }, [mouseCoord,dimensions.x1,dimensions.y1]);
-
-  useEffect(()=>{
-   
-
-  },[])
-
-function mouseHandler(event:MouseEvent){
-  setMouseCoord({x:event.offsetX,y:event.offsetY})
-  setDimesions({x1:event.clientX,y1:event.clientY,x2:event.offsetX,y2:event.offsetY})
-  console.log(event)
-}
+    const ctx = c?.getContext("2d");
+    ctx.lineWidth=10
+    ctx.lineCap="round"
+    ctx.lineTo(e.clientX,e.clientY)
+    ctx.stroke()
+    console.log(e)
+  })
+  
+},[mouseCoord,painting])
   
 
   return (
     <div>
-      <canvas className="h-[50vh] w-[50vw] ring-2 ring-amber-500 m-32" id="myCanvas" onMouseDown={(e)=>{
-      // console.log(e)
-      
-    e.target.addEventListener("mousemove",mouseHandler)
-
-    e.target.addEventListener("mouseleave",(g)=>{
-      e.target.removeEventListener("mousemove",mouseHandler)
-    })
-    
-    e.target.addEventListener("mouseup",(h)=>{
-      e.target.removeEventListener("mousedown",mouseHandler)
-    })
-
-      } 
-}></canvas>
+      <canvas className="h-[768px] w-[1366px] ring-2 ring-amber-500 m-32" id="myCanvas" ></canvas>
     </div>
   );
 }
